460ef5b87611700bf9d7d0b8bb70a82fcc59ba0b
Zach Gray <zcgray@southern.edu>
2023-02-20 12:18:12 -0500

    failed treading attempts

--- simple_tool/sp_autosave.py.py
+++ simple_tool/sp_autosave.py.py
@@ -1,77 +1,93 @@
-import maya.cmds as mc
 import threading
 import winsound
+import maya.cmds as mc
 
-def setInterval():
-    def decorator(function):
-        def wrapper(*args, **kwargs):
-            stopped = threading.Event()
+'''
+class TimerThread(threading.Thread):
+    def __init__(self, duration, callback):
+        super().__init__()
+        self.duration = duration
+        self.callback = callback
+        self._stop_event = threading.Event()
 
-            def loop(): # executed in another thread
-                global auto_save_interval
-                interval = auto_save_interval
-                while not stopped.wait(interval): # until stopped
-                    function(*args, **kwargs)
+    def run(self):
+        while not self._stop_event.is_set():
+            self._stop_event.wait(self.duration)
+            self.callback()
 
-            t = threading.Thread(target=loop)
-            t.daemon = True # stop if the program exits
-            t.start()
-            return stopped
-        return wrapper
-    return decorator
+    def stop(self):
+        self._stop_event.set()
+        
+'''
 
-global auto_save_interval
-global auto_save_stop
-auto_save_interval = 1
-auto_save_stop = None
+class TimerThread(threading.Thread):
+    def __init__(self, duration, callback):
+        super().__init__()
+        self.duration = duration
+        self.callback = callback
+        self._stop_event = threading.Event()
+        self._stop_requested = False
 
+    def run(self):
+        while not self._stop_event.is_set():
+            self._stop_event.wait(self.duration)
+            if self._stop_requested:
+                break
+            self.callback()
 
-@setInterval()
-def auto_save_prompt():
+    def stop(self):
+        self._stop_requested = True
+
+def timer_callback():
+    global timer_thread
+    print("callback")
+    print(threading.get_ident())
+    
     winsound.Beep(529, 500)
-    # create a modal confirm dialog that says
-    # https://help.autodesk.com/cloudhelp/2023/ENU/Maya-Tech-Docs/CommandsPython/promptDialog.html
 
-    
-    # always stop on each cycle so we can reset the interval
-    global auto_save_stop
-    global auto_save_interval 
-    auto_save_stop.set()
 
-    '''
-    interval = 10
+    # timer_thread.stop()
+
     result = mc.promptDialog(
-		title='Autosave reminder',
-		message='Enter Duration:',
-        tx = str(auto_save_interval),  
-		button=['Save', 'Wait'],
-		defaultButton='OK',
-		cancelButton='Wait',
-		dismissString='Kill')
-    
-    text = mc.promptDialog(query=True, text=True)
-    if text.isdigit():
-        interval = int(text)
+        title='Save prompt',
+        message='Enter Seconds:',
+        button=['OK', 'Cancel'],
+        defaultButton='OK',
+        tx = str(timer_thread.duration),
+        cancelButton='Cancel',
+        dismissString='Cancel')
 
-    if result == 'Save':
-        # put save code here
-        print('saving')
-        setup_autosave(interval)
-    elif result == 'Wait':
-        print('waiting')
-        setup_autosave(interval)
+    if result == 'OK':
+        print("result ok")
+        print(threading.get_ident())
+        text = mc.promptDialog(query=True, text=True)
+        text = [int(i) for i in text.split() if i.isdigit()][0]
+        new_duration = timer_thread.duration
+        if text:
+            new_duration = int(text)
+        timer_thread.duration = new_duration
     else:
-        print ('stopping')
-    '''
+        print("stop")
+        print(threading.get_ident())
+        # stop_timer()
+        timer_thread.stop()
+        winsound.Beep(129, 1000)
 
-    
- # start timer, the first call is in .5 seconds
-def setup_autosave(interval):
-    global auto_save_interval
-    global auto_save_stop
-    auto_save_interval = interval
-    auto_save_stop = auto_save_prompt()
- # stop 
-#stop.set()
-# auto_save_stop.set()
-setup_autosave(5)
+def setup_timer(duration=3):
+    global timer_thread
+    timer_thread = TimerThread(duration, timer_callback)
+    timer_thread.start()
+    print('setup')
+    print(threading.get_ident())
+
+def stop_timer():
+    print('stopping')
+    print(threading.get_ident())
+    global timer_thread
+    timer_thread.stop()    
+    winsound.Beep(129, 1000)
+
+if __name__ == '__main__':
+    print('main')
+    print(threading.get_ident())
+    setup_timer()
