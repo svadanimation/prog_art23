2db91812de18b9bfe17a9b93ba7b44c1fbadd710
nathanaelperez <nathanaelperez@southern.edu>
2023-02-23 14:34:52 -0500

    Updated Functions and If/Name block.

--- file_io/np_file_io.py
+++ file_io/np_file_io.py
@@ -5,47 +5,53 @@ import re
 import pprint as pprint
 
 #Constants
-directory = "Z:"
-text_file = "text_file.text"
-json_file = "json_file.json"
+directory = 'Z:'
+text_file = 'text_file.txt'
+json_file = 'json_file.json'
 
 #Paths
-text_path = os.path.join(directory, "//Nathanael Perez/Classes/W23/Programming for Artists/", text_file)
-json_path = os.path.join(directory, "//Nathanael Perez/Classes/W23/Programming for Artists/", json_file)
+text_path = os.path.join(directory + text_file)
+json_path = os.path.join(directory + json_file)
 
 #Variables
-text = "abc123"
-data = {"a": 1, "b": 2, "c": 3, "d": 4, "e": {"everyman": 999}}
+text = 'abc123'
+data = {
+        'a': 1, 
+        'b': 2, 
+        'c': 3, 
+        'd': 4, 
+        'e': {'egg': 666, 'everyman': 999}
+        }
 
 #Text Functions
-def rw_text(path):
-    def write_text(path):
-        with open(path, "w") as t:
+def rw_text(path, w = True):
+    text_info = None
+    if w:
+        with open(path, 'w') as t:
             t.write(text)
-        text_info = None
-    def read_text(path):
-        with open(path, "r") as t:
+    else:
+        with open(path, 'r') as t:
             for line in t:  
-                match = re.match(r"([a-z]+)([0-9]+)", line)
+                match = re.match(r'([a-z]+)([0-9]+)', line)
             if match:
                 items = match.groups()
             text_info = t.read()
-        print(text_info)
-        #return text_info
+            return text_info
 
 #Json Functions
-def rw_json(path):
-    def write_json(path):
-        with open(path, "w") as j:
+def rw_json(path, w = True):
+    json_info = None
+    if w:
+         with open(path, 'w') as j:
             json.dump(data, j)
-        json_info = None
-    def read_json(path):
-        with open(path, "r") as j:
+    else:
+        with open(path, 'r') as j:
             json_info = json.load(j)
-        print(json_info)
-        #return json_info
+            return json_info
 
 #Script Execution
-if __name__ == "__main__":
-    rw_text(text_path)
-    rw_json(json_path)
+if __name__ == '__main__':
+    rw_text(text_path, True)
+    print(rw_text(text_path, False))
+    rw_json(json_path, True)
+    print(rw_json(json_path, False))
