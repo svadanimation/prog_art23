56e44d5da4d791b8d43d248a92d78df956492feb
President-Pufferfish <124204085+President-Pufferfish@users.noreply.github.com>
2023-02-27 12:19:50 -0500

    Update am_main.py

--- file_io/am_main.py
+++ file_io/am_main.py
@@ -1,23 +1,25 @@
 import json
 import os
 
-filepath = os.path.join(os.path.realpath(C:\Visual Studio\text.json))
-filepath2 = os.path.join(os.path.realpath(C:\Visual Studio\text.txt))
+filepath = os.path.join(os.path.realpath('C:\Visual Studio\text.json'))
+filepath2 = os.path.join(os.path.realpath('C:\Visual Studio\text.txt'))
 
 with open("text.json","w") as f:
-    f.write("Google it"\n)
+    f.write("Google it")
 dictionary = {1:"one",
 2:"two",3:"three", 4:"four",5:"five", 6:{1:"2nd one",2:"2nd Two"}}
 
-with open(filepath, 'w') as f:
+with open('filepath', 'w') as f:
     json.dump(dictionary, f)
 
-with open("filepath", "r") as f:
-content = f.read()
+with open('filepath', 'r') as f:
+    content = f.read()
 
+name = 'main'
 if name == 'main':
     print("Always executed")
 else:
     print("Executed when imported")
+
 
 AKM2_file_io.py
