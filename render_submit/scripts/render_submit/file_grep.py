54632c10b7dad15ed4a9f594bc1c5e89081f0c0e
nathanaelperez <nathanaelperez@southern.edu>
2023-02-27 15:43:33 -0500

    Finished grep portion of module.

--- render_submit/scripts/render_submit/file_grep.py
+++ render_submit/scripts/render_submit/file_grep.py
@@ -1,1 +1,22 @@
-pass
+'''
+Module to grep and sort files.
+'''
+
+import glob
+import os 
+import time
+
+def get_files(directory, types = ('*.ma', '*.mb')):
+    directory = os.path.abspath(directory)
+    files = []
+    for type in types:
+        # files is the list of .ma and .mb files
+        file_paths = glob.glob(os.path.join(directory, '**', type), recursive=True)
+        for file_path in file_paths:
+            file_time = time.ctime(os.path.getmtime(file_path))
+            files.append((file_path, file_time))
+    return files
+
+if __name__ == '__main__':
+    directory = 'Z:'
+    print(get_files(directory))
